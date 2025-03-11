fac345d276c699542b3d41e12ff11bfeb0e2cf59
Mac-Kay-UABC <p372063@uabc.edu.mx>
2025-03-11 10:48:37 -0700

    Se hizo el cambio de codigo en GitHub

--- app/Code3.py
+++ app/Code3.py
@@ -1,7 +1,7 @@
 import random
 
-def roll_dice(sides=6):
+def roll_dice(sides=20):
     return random.randint(1, sides)
 
-print("Rolling a 6-sided die...")
+print("Rolling a 20-sided die...")
 print(f"You rolled a {roll_dice()}")
